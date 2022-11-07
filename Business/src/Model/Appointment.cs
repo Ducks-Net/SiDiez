2519dd2467bc7541f4dd18e81770758de3ddc5d0
= <=>
2022-11-07 17:04:53 +0200

    [Update] Appointment, CLinic and Validate

--- Business/src/Model/Appointment.cs
+++ Business/src/Model/Appointment.cs
@@ -5,17 +5,18 @@ using VetAppointment.Util;
 class Appointment {
     Owner Owner { get; }
     Client Client { get; }
+    Clinic Clinic { get; }
     List<Employee> Employees { get; }
     DateTime StartTime { get; }
     DateTime EndTime { get; }
     string Description { get; }
 
     //TODO(AL): invoice
-
-    private Appointment(Owner owner, Client client, List<Employee> employees, DateTime start, DateTime end, string description) 
+    private Appointment(Owner owner, Client client, Clinic clinic,  List<Employee> employees, DateTime start, DateTime end, string description) 
     {
          this.Owner = owner;
          this.Client = client;
+         this.Clinic = clinic;
          this.Employees = employees;
          this.StartTime = start;
          this.EndTime = end;
@@ -25,11 +26,12 @@ class Appointment {
 
     //TODO(AL): maybe standardise descriptions or add another field to standardise
     //TODO(AL): prefered to overload with just ids
-    public Result<Appointment> Create(Owner owner, Client client, List<Employee> employees, DateTime start, DateTime end, string description)
+    public Result<Appointment> Create(Owner owner, Client client, Clinic clinic, List<Employee> employees, DateTime start, DateTime end, string description)
     {
         return Result<Appointment>.Success(new Appointment(
             owner,
             client,
+            clinic,
             employees,
             start,
             end,
--- Business/src/Util/Validate.cs
+++ Business/src/Util/Validate.cs
@@ -9,4 +9,17 @@ class Validate{
         result = result && (phoneNumber.Any(x => !char.IsLetter(x)));
         return result;
     }
+    static public bool ValidateEmail(string email){
+        var trimmedEmail = email.Trim();
+        if (trimmedEmail.EndsWith(".")) {
+            return false; 
+        }
+        try {
+            var addr = new System.Net.Mail.MailAddress(email);
+            return addr.Address == trimmedEmail;
+        }
+        catch {
+            return false;
+        }
+    }
 }
