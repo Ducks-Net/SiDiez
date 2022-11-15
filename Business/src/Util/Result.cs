df481a3b0d6ab7bbdae00208c1905d07777d244f
= <=>
2022-11-15 21:55:25 +0200

    Reworked Result<T> to be more robust

--- Business/src/Util/Result.cs
+++ Business/src/Util/Result.cs
@@ -1,34 +1,65 @@
 namespace VetAppointment.Util;
 
+public class Result
+{
+    public string? Error { get; private set; }
+    public bool IsSuccess { get; private set; }
+    public bool IsFailure { get; private set; }
 
-// TODO(AL): make this less bad
-public class Result {
-    public string Error{ get; private set;}
-    public bool IsSuccess { get; private set;}
-    public bool IsFailure { get; private set;}
-
-
-
-    public static Result Success() {
-        return new Result() { IsSuccess = true, IsFailure = false };
+    private Result(string errorMessage) {
+        this.Error = errorMessage;
+        this.IsSuccess = false;
+        this.IsFailure = true;
     }
 
-    public static Result Failure(string Error) {
-        return new Result() { Error = Error, IsFailure = true, IsSuccess = false };
+    private Result() {
+        this.Error = null;
+        this.IsSuccess = true;
+        this.IsFailure = false;
+    }
+
+    public static Result Success()
+    {
+        return new Result();
+    }
+
+    public static Result Failure(string errorMessage)
+    {
+        return new Result(errorMessage);
     }
 }
 
-public class Result<EntityT> {
-    public EntityT Entity { get; private set; }
-    public string Error{ get; private set;}
-    public bool IsSuccess { get; private set;}
-    public bool IsFailure { get; private set;}
+public class Result<T>
+{
+    public T? Entity { get; private set; }
+    public string? Error { get; private set; }
+    public bool IsSuccess { get; private set; }
+    public bool IsFailure { get; private set; }
 
-    public static Result<EntityT> Success(EntityT ok) {
-        return new Result<EntityT>() { Entity = ok, IsSuccess = true, IsFailure = false };
+    private Result(T entity)
+    {
+        this.Entity = entity;
+        this.Error = null;
+        this.IsSuccess = true;
+        this.IsFailure = false;
     }
 
-    public static Result<EntityT> Failure(string Error) {
-        return new Result<EntityT>() { Error = Error, IsFailure = true, IsSuccess = false };
+    //NOTE(AL): the `bool failed` here is basically just a tag to diferentiate between the overloads in case T =
+    private Result(string error, bool failed)
+    {
+        this.Entity = default(T); //FIXME(AL): this is not nice. but I don't want to restrict T to only reference types.
+        this.Error = error;
+        this.IsSuccess = false;
+        this.IsFailure = true;
+    }
+
+    public static Result<T> Success(T ok)
+    {
+        return new Result<T>(ok);
+    }
+
+    public static Result<T> Failure(string errorMessage)
+    {
+        return new Result<T>(errorMessage, true);
     }
 }
