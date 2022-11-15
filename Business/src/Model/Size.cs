dfc35d9f15ca23bac2f2c764dd1b8cb00f22352c
= <=>
2022-11-15 21:10:24 +0200

    [Added] Method to parse string to Result<Size>
    Simple util to make error handling easier.

--- Business/src/Model/Size.cs
+++ Business/src/Model/Size.cs
@@ -1,5 +1,30 @@
-public enum Size{
-    Small, 
+using VetAppointment.Util;
+
+public enum Size
+{
+    Small,
     Medium,
     Big
-}
+};
+
+
+public static class SizeExtensions
+{
+
+    //Note(AL): kinda sad that we can't add this method to the `class` proper...
+    public static Result<Size> FromString(string size)
+    {
+        Size parsedSize;
+        bool parsedSuccefully = Size.TryParse(size, out parsedSize);
+
+        if (!parsedSuccefully)
+        {
+            // Error case:
+            Array possibleSizeValues = Enum.GetValues(typeof(Size));
+            string errorMessage = $"Failed to parse '{size}' as Size. Possible values for Size are: {possibleSizeValues}";
+            return Result<Size>.Failure(errorMessage);
+        }
+
+        return Result<Size>.Success(parsedSize);
+    }
+};
