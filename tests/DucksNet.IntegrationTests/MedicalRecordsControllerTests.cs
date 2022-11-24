e1eed0382e4785e7f232f030f35b77027b59608a
Aldea Andrei <aldea.andrei.977@gmail.com>
2022-11-24 22:59:10 +0200

    2 tests on testdbcontext

--- tests/DucksNet.IntegrationTests/MedicalRecordsControllerTests.cs
+++ tests/DucksNet.IntegrationTests/MedicalRecordsControllerTests.cs
@@ -2,7 +2,11 @@
 using System.Threading.Tasks;
 using DucksNet.API.Controllers;
 using DucksNet.API.DTO;
+using DucksNet.Infrastructure.Prelude;
+using DucksNet.Infrastructure.Sqlite;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
 using static System.Guid;
 
 namespace DucksNet.API.Integration_Tests;
@@ -12,7 +16,11 @@ public class MedicalRecordsControllerTests
     [Fact]
     public async Task When_CreatedMedicalRecord_Then_ShouldReturnMedicalRecordInTheGetRequest()
     {
-        await using var application = new WebApplicationFactory<DucksNet.API.Controllers.MedicalRecordsController>();
+        await using var application = new WebApplicationFactory<DucksNet.API.Controllers.MedicalRecordsController>().WithWebHostBuilder(builder => { 
+            builder.ConfigureTestServices(services => {
+                services.AddScoped<IDatabaseContext>(provider => new TestDbContext("MedicalRecordsControllerTests"));
+            });
+        });
         using var client = application.CreateClient();
 
         //Arrange
