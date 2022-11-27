fd95a5d7f4d8ddc82b5791efae12488531ebb346
OpariucRares <46899212+OpariucRares@users.noreply.github.com>
2022-11-27 18:51:41 +0200

    added Validation (AGAIN)

---
+++ src/DucksNet.SharedKernel/Utils/Validation.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DucksNet.SharedKernel.Utils;
+public class Validation
+{
+    public static bool IsTelephoneNumberValid(string phone)
+    {
+        Regex validatePhoneNumber = new Regex("^(\\+4|)?(07[0-8]{1}[0-9]{1}|02[0-9]{2}|03[0-9]{2}){1}?(\\s|\\.|\\-)?([0-9]{3}(\\s|\\.|\\-|)){2}$");
+        return validatePhoneNumber.IsMatch(phone);
+    }
+    public static bool IsEmailValid(string email)
+    {
+        Regex validateEmail = new Regex("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
+        return validateEmail.IsMatch(email);
+    }
+}
