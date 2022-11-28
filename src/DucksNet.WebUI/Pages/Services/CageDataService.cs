7231d4906ff9ce8368b90817f9c17cce95230246
Aldea Andrei <aldea.andrei.977@gmail.com>
2022-11-28 13:00:54 +0200

    cages page fail

---
+++ src/DucksNet.WebUI/Pages/Services/CageDataService.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http.Json;
+using DucksNet.Domain.Model;
+
+namespace DucksNet.WebUI.Pages.Services;
+
+public class CageDataService : ICageDataService
+{
+    private const string ApiURL = "https://localhost:7115/api/v1/cages";
+    private readonly HttpClient httpClient;
+
+    public CageDataService(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+
+    public Task<IEnumerable<Cage>> GetAllCages()
+    {
+        return httpClient.GetAsync(ApiURL).Result.Content.ReadFromJsonAsync<IEnumerable<Cage>>()!;
+    }
+
+    public Task<Cage> GetCageDetail(Guid cageId)
+    {
+        throw new NotImplementedException();
+    }
+}
