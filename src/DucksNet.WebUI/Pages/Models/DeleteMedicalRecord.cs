0a8c8e40903ca4c6fa917b9caa7c15c2a2b7c0b3
OpariucRares <46899212+OpariucRares@users.noreply.github.com>
2022-12-05 01:23:14 +0200

    added UI interface for medical Record and made the MedicalRecord 100%

--- src/DucksNet.WebUI/Pages/Models/DeleteMedicalRecord.cs
+++ src/DucksNet.WebUI/Pages/Models/DeleteMedicalRecord.cs
@@ -6,5 +6,5 @@ public class DeleteMedicalRecord
 {
     [Required]
     [StringLength(36, MinimumLength = 36, ErrorMessage = "Guid must be 36 characters long.")]
-    public string? EmployeeId { get; set; }
+    public string? MedicalRecordId { get; set; }
 }
