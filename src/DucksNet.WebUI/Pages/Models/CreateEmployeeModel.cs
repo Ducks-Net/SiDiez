6b42ec205ebc53e8cc1c1cd2f5ef481ee356d5c8
OpariucRares <46899212+OpariucRares@users.noreply.github.com>
2022-12-04 20:33:22 +0200

    changed the minimumLength to 2

--- src/DucksNet.WebUI/Pages/Models/CreateEmployeeModel.cs
+++ src/DucksNet.WebUI/Pages/Models/CreateEmployeeModel.cs
@@ -8,13 +8,13 @@ public class CreateEmployeeModel
     [StringLength(36, MinimumLength = 36, ErrorMessage = "Guid must be 36 characters long.")]
     public string? IdOffice { get; set; }
     [Required]
-    [StringLength(120, MinimumLength = 5, ErrorMessage = "Surname must be at least 5 characters long.")]
+    [StringLength(120, MinimumLength = 2, ErrorMessage = "Surname must be at least 5 characters long.")]
     public string? Surname { get; set; }
     [Required]
-    [StringLength(120, MinimumLength = 5, ErrorMessage = "First Name must be at least 5 characters long.")]
+    [StringLength(120, MinimumLength = 2, ErrorMessage = "First Name must be at least 5 characters long.")]
     public string? FirstName { get; set; }
     [Required]
-    [StringLength(120, MinimumLength = 5, ErrorMessage = "Addres must be at least 5 characters long.")]
+    [StringLength(120, MinimumLength = 2, ErrorMessage = "Addres must be at least 5 characters long.")]
     public string? Address { get; set; }
     [Required]
     public string? OwnerPhone { get; set; }
