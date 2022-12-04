1fd0e857d95d06f30ef8ccee4737bced6d2c3dc6
OpariucRares <46899212+OpariucRares@users.noreply.github.com>
2022-12-04 23:21:17 +0200

    created Models for MedicalRecord

---
+++ src/DucksNet.WebUI/Pages/Models/DeleteMedicalRecord.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DucksNet.WebUI.Pages.Models;
+
+public class DeleteMedicalRecord
+{
+    [Required]
+    [StringLength(36, MinimumLength = 36, ErrorMessage = "Guid must be 36 characters long.")]
+    public string? EmployeeId { get; set; }
+}
