b3a1a8e85e255bb8fd8045bfb608bab31e594fdf
OpariucRares <46899212+OpariucRares@users.noreply.github.com>
2022-12-05 10:02:18 +0200

    code factor bs

--- src/DucksNet.WebUI/Pages/Models/CreatePetModel.cs
+++ src/DucksNet.WebUI/Pages/Models/CreatePetModel.cs
@@ -4,7 +4,6 @@ namespace DucksNet.WebUI.Pages.Models;
 
 public class CreatePetModel
 {
-
     [Required]
     [StringLength(120, MinimumLength = 2, ErrorMessage = "Name must be at least 2 characters long.")]
     public string? Name { get; set; }
