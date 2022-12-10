46569deea596308d623294c783a145728c28ee3f
Aldea Andrei <aldea.andrei.977@gmail.com>
2022-12-10 22:00:29 +0200

    Elide a move in the test runner script

--- RunTests.ps1
+++ RunTests.ps1
@@ -1,6 +1,5 @@
 $tests = @("AppointmentsController", "CagesController", "EmployeesController", "MedicalRecordsController", "MedicineController", "TreatmentController")
 
 foreach ($test in $tests) {
-    dotnet-coverage collect "dotnet test --filter 'FullyQualifiedName~DucksNet.IntegrationTests.$($test)'" -f xml -o 'coverage.xml'
-    Move-Item tests/DucksNet.IntegrationTests/coverage.xml tests/DucksNet.IntegrationTests/$($test).coverage.xml; \
+    dotnet-coverage collect "dotnet test --filter 'FullyQualifiedName~DucksNet.IntegrationTests.$($test)'" -f xml -o "tests/DucksNet.IntegrationTests/$($test).coverage.xml"
 }
