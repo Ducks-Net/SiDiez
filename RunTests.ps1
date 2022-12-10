23eb8dc9d3a18f93775a019dce6e63209080f3cd
Aldea Andrei <aldea.andrei.977@gmail.com>
2022-12-10 21:50:47 +0200

    Fix string interpolation

--- RunTests.ps1
+++ RunTests.ps1
@@ -1,9 +1,6 @@
-$strings = @("AppointmentsController", "CagesController", "EmployeesController", "MedicalRecordsController", "MedicineController", "TreatmentController")
+$tests = @("AppointmentsController", "CagesController", "EmployeesController", "MedicalRecordsController", "MedicineController", "TreatmentController")
 
-# Loop over the array of strings
-foreach ($string in $strings) {
-    # Call a command on each string
-    # dotnet-coverage collec"t 'dotnet test' -f xml -o 'coverage.xml'
-    dotnet-coverage collect "dotnet test --filter 'FullyQualifiedName~DucksNet.IntegrationTests.$(string)'" -f xml -o 'coverage.xml'
-    Move-Item tests/DucksNet.IntegrationTests/coverage.xml tests/DucksNet.IntegrationTests/$(string).coverage.xml; \
+foreach ($test in $tests) {
+    dotnet-coverage collect "dotnet test --filter 'FullyQualifiedName~DucksNet.IntegrationTests.$($test)'" -f xml -o 'coverage.xml'
+    Move-Item tests/DucksNet.IntegrationTests/coverage.xml tests/DucksNet.IntegrationTests/$($test).coverage.xml; \
 }
