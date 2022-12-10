6cd0dc1d3b6e10c3d2ced87a0d151aed92b8eaf1
Aldea Andrei <aldea.andrei.977@gmail.com>
2022-12-10 22:15:38 +0200

    Fix type

--- RunTests.ps1
+++ RunTests.ps1
@@ -1,5 +1,5 @@
 $tests = @("AppointmentsController", "CagesController", "EmployeesController", "MedicalRecordsController", "MedicineController", "TreatmentController")
 
 foreach ($test in $tests) {
-    dotnet-coverage collect "dotnet test --filter 'FullyQualifiedName~DucksNet.IntegrationTests.$($test)'" -f xml -o "tests/DucksNet.IntegrationTests/$($test).coverage.xml"
+    dotnet-coverage collect "dotnet test --filter 'FullyQualifiedName~DucksNet.IntegrationTests.$($test)Tests'" -f xml -o "tests/DucksNet.IntegrationTests/$($test).coverage.xml"
 }
