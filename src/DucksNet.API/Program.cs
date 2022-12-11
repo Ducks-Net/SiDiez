8b8837d693299852d6e47dac4c09db64475e609b
Aldea Andrei <aldea.andrei.977@gmail.com>
2022-12-11 10:06:34 +0200

    Added async repositories

--- src/DucksNet.API/Program.cs
+++ src/DucksNet.API/Program.cs
@@ -21,6 +21,18 @@ builder.Services.AddScoped<IRepository<Employee>, EmployeeRepository>();
 builder.Services.AddScoped<IRepository<Business>, BusinessesRepository>();
 builder.Services.AddScoped<IRepository<Office>, OfficesRepository>();
 
+builder.Services.AddScoped<IRepositoryAsync<Cage>, RepositoryAsync<Cage>>();
+builder.Services.AddScoped<IRepositoryAsync<CageTimeBlock>, RepositoryAsync<CageTimeBlock>>();
+builder.Services.AddScoped<IRepositoryAsync<Appointment>, RepositoryAsync<Appointment>>();
+builder.Services.AddScoped<IRepositoryAsync<Pet>, RepositoryAsync<Pet>>();
+builder.Services.AddScoped<IRepositoryAsync<User>, RepositoryAsync<User>>();
+builder.Services.AddScoped<IRepositoryAsync<Treatment>, RepositoryAsync<Treatment>>();
+builder.Services.AddScoped<IRepositoryAsync<Medicine>, RepositoryAsync<Medicine>>();
+builder.Services.AddScoped<IRepositoryAsync<MedicalRecord>, RepositoryAsync<MedicalRecord>>();
+builder.Services.AddScoped<IRepositoryAsync<Employee>, RepositoryAsync<Employee>>();
+builder.Services.AddScoped<IRepositoryAsync<Business>, RepositoryAsync<Business>>();
+builder.Services.AddScoped<IRepositoryAsync<Office>, RepositoryAsync<Office>>();
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("VetPolicyCors",policy =>
---
+++ src/DucksNet.Infrastructure/SqliteAsync/RepositoryAsync.cs
@@ -0,0 +1,53 @@
+using DucksNet.Infrastructure.Prelude;
+using DucksNet.SharedKernel.Utils;
+using Microsoft.EntityFrameworkCore;
+
+namespace DucksNet.Infrastructure.Sqlite;
+
+public class RepositoryAsync<T> : IRepositoryAsync<T> where T : class
+{
+
+    private readonly IDatabaseContext _context;
+    
+    public RepositoryAsync(IDatabaseContext context)
+    {
+        this._context = context;
+    }
+
+    public async Task<Result<T>> GetAsync(Guid id)
+    {
+        var entity = await _context.Set<T>().FindAsync(id);
+        if (entity == null)
+        {
+            return Result<T>.Error($"Entity of type {typeof(T).Name} with id {id} was not found");
+        }
+
+        return Result<T>.Ok(entity);
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+
+    public async Task<Result> AddAsync(T entity)
+    {
+        await _context.Set<T>().AddAsync(entity);
+        await _context.SaveChangesAsync();
+        return Result.Ok();
+    }
+    
+    public async Task<Result> UpdateAsync(T entity)
+    {
+        _context.Set<T>().Update(entity);
+        await _context.SaveChangesAsync();
+        return Result.Ok();
+    }
+    
+    public async Task<Result> DeleteAsync(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+        await _context.SaveChangesAsync();
+        return Result.Ok();
+    }
+}
