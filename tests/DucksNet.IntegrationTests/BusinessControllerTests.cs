a8930aa9f4ed0c519b15a6ccc1aec53cc54695aa
ndrey12 <75525934+ndrey12@users.noreply.github.com>
2022-12-11 21:43:14 +0200

    Update BusinessControllerTests.cs

--- tests/DucksNet.IntegrationTests/BusinessControllerTests.cs
+++ tests/DucksNet.IntegrationTests/BusinessControllerTests.cs
@@ -30,7 +30,6 @@ public class BusinessControllerTests : BaseIntegrationTests<BusinessController>
         var response =  TestingClient.PostAsJsonAsync(businessUrl, business).Result;
 
         response.StatusCode.Should().Be(HttpStatusCode.OK);
-        //System.Console.WriteLine(response.Content.ReadAsStringAsync().Result);
         var result = response.Content.ReadFromJsonAsync<Business>().Result;
         result.Should().NotBeNull();
         result!.BusinessName.Should().Be(business.BusinessName);
@@ -235,4 +234,225 @@ public class BusinessControllerTests : BaseIntegrationTests<BusinessController>
         result.OwnerPhone.Should().Be(business.OwnerPhone);
         result.OwnerEmail.Should().Be(business.OwnerEmail);
     }
-}
+    [Fact] 
+    public void When_GetAll_Should_ReturnAllValues()
+    {
+        ClearDatabase();
+        var business = new BusinessDTO
+        {
+            BusinessName = "BusinessName",
+            Surname = "Surname",
+            FirstName = "FirstName",
+            Address = "Address",
+            OwnerPhone = "0734567890",
+            OwnerEmail = "ion@e.com",
+        };
+
+        var response =  TestingClient.PostAsJsonAsync(businessUrl, business).Result;
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var result = response.Content.ReadFromJsonAsync<Business>().Result;
+
+        result.Should().NotBeNull();
+        result!.BusinessName.Should().Be(business.BusinessName);
+        result.Surname.Should().Be(business.Surname);
+        result.FirstName.Should().Be(business.FirstName);
+        result.Address.Should().Be(business.Address);
+        result.OwnerPhone.Should().Be(business.OwnerPhone);
+        result.OwnerEmail.Should().Be(business.OwnerEmail);
+    }
+    [Fact]
+    public void When_GetAll_Should_ReturnAllValues2()
+    {
+        ClearDatabase();
+        var business = new BusinessDTO
+        {
+            BusinessName = "BusinessName",
+            Surname = "Surname",
+            FirstName = "FirstName",
+            Address = "Address",
+            OwnerPhone = "0731678902",
+            OwnerEmail = "ion@e.com",
+        };
+
+        var response =  TestingClient.PostAsJsonAsync(businessUrl, business).Result;
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var result = response.Content.ReadFromJsonAsync<Business>().Result;
+
+        result.Should().NotBeNull();
+
+        var business2 = new BusinessDTO
+        {
+            BusinessName = "BusinessName2",
+            Surname = "Surname2",
+            FirstName = "FirstName2",
+            Address = "Address2",
+            OwnerPhone = "0734678902",
+            OwnerEmail = "aaasd@i.com",
+        };
+
+        var response2 =  TestingClient.PostAsJsonAsync(businessUrl, business2).Result;
+
+        response2.StatusCode.Should().Be(HttpStatusCode.OK);
+        var result2 = response2.Content.ReadFromJsonAsync<Business>().Result;
+
+        result2.Should().NotBeNull();
+
+        var response3 =  TestingClient.GetAsync(businessUrl).Result;
+
+        response3.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var result3 = response3.Content.ReadFromJsonAsync<List<Business>>().Result;
+
+        result3.Should().NotBeNull();
+        result3!.Count.Should().Be(2);
+        result3[0].BusinessName.Should().Be(business.BusinessName);
+        result3[0].Surname.Should().Be(business.Surname);
+        result3[0].FirstName.Should().Be(business.FirstName);
+        result3[0].Address.Should().Be(business.Address);
+        result3[0].OwnerPhone.Should().Be(business.OwnerPhone);
+        result3[0].OwnerEmail.Should().Be(business.OwnerEmail);
+        result3[1].BusinessName.Should().Be(business2.BusinessName);
+        result3[1].Surname.Should().Be(business2.Surname);
+        result3[1].FirstName.Should().Be(business2.FirstName);
+        result3[1].Address.Should().Be(business2.Address);
+        result3[1].OwnerPhone.Should().Be(business2.OwnerPhone);
+        result3[1].OwnerEmail.Should().Be(business2.OwnerEmail);
+    }
+    [Fact]
+    public void When_GetAll_Should_ReturnAllValues3()
+    {
+        ClearDatabase();
+        var business = new BusinessDTO
+        {
+            BusinessName = "BusinessName",
+            Surname = "Surname",
+            FirstName = "FirstName",
+            Address = "Address",
+            OwnerPhone = "0731678902",
+            OwnerEmail = "asd@asd.com",
+        };
+
+        var response =  TestingClient.PostAsJsonAsync(businessUrl, business).Result;
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var result = response.Content.ReadFromJsonAsync<Business>().Result;
+
+        result.Should().NotBeNull();
+
+        var business2 = new BusinessDTO
+        {
+            BusinessName = "BusinessName2",
+            Surname = "Surname2",
+            FirstName = "FirstName2",
+            Address = "Address2",
+            OwnerPhone = "0734678902",
+            OwnerEmail = "",
+        
+        };
+
+        var response2 =  TestingClient.PostAsJsonAsync(businessUrl, business2).Result;
+
+        response2.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var errors = response2.Content.ReadFromJsonAsync<List<string>>().Result;
+
+        errors.Should().NotBeNull();
+
+        errors.Count.Should().Be(1);
+        errors[0].Should().Be("Owner email is required");
+    }
+    [Fact]
+    public void When_GetAll_Should_ReturnAllValues4()
+    {
+        ClearDatabase();
+        var business = new BusinessDTO
+        {
+            BusinessName = "BusinessName",
+            Surname = "Surname",
+            FirstName = "FirstName",
+            Address = "Address",
+            OwnerPhone = "0731678902",
+            OwnerEmail = "asd@a.com",
+        };
+
+        var response =  TestingClient.PostAsJsonAsync(businessUrl, business).Result;
+        
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var result = response.Content.ReadFromJsonAsync<Business>().Result;
+
+        result.Should().NotBeNull();
+
+        var business2 = new BusinessDTO
+        {
+            BusinessName = "BusinessName2",
+            Surname = "Surname2",
+            FirstName = "FirstName2",
+            Address = "Address2",
+            OwnerPhone = "0734678902",
+            OwnerEmail = "asedasd",
+        };
+
+        var response2 =  TestingClient.PostAsJsonAsync(businessUrl, business2).Result;
+
+        response2.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var errors = response2.Content.ReadFromJsonAsync<List<string>>().Result;
+
+        errors.Should().NotBeNull();
+
+        errors.Count.Should().Be(1);
+
+        errors[0].Should().Be("Owner email is invalid");
+    }
+    [Fact]
+    public void When_GetAll_Should_ReturnAllValues5()
+    {
+        ClearDatabase();
+        var business = new BusinessDTO
+        {
+            BusinessName = "BusinessName",
+            Surname = "Surname",
+            FirstName = "FirstName",
+            Address = "Address",
+            OwnerPhone = "0731678902",
+            OwnerEmail = "a@asd.comasd",
+        };
+
+        var response =  TestingClient.PostAsJsonAsync(businessUrl, business).Result;
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var result = response.Content.ReadFromJsonAsync<Business>().Result;
+
+        result.Should().NotBeNull();
+
+        var business2 = new BusinessDTO
+        {
+            BusinessName = "BusinessName2",
+            Surname = "Surname2",
+            FirstName = "FirstName2",
+            Address = "Address2",
+            OwnerPhone = "0734678902",
+            OwnerEmail = "asd.asd",
+        };
+
+        var response2 =  TestingClient.PostAsJsonAsync(businessUrl, business2).Result;
+
+        response2.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var errors = response2.Content.ReadFromJsonAsync<List<string>>().Result;
+
+        errors.Should().NotBeNull();
+
+        errors.Count.Should().Be(1);
+
+        errors[0].Should().Be("Owner email is invalid");
+    }
+        
+}       
