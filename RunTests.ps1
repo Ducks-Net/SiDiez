f2bef4d6800b20c6309670a3e02a3632cd552555
Aldea Andrei <aldea.andrei.977@gmail.com>
2022-12-11 06:54:05 +0200

    Try mergign the reports into 1

--- RunTests.ps1
+++ RunTests.ps1
@@ -1,5 +1,7 @@
 $tests = @("AppointmentsController", "CagesController", "EmployeesController", "MedicalRecordsController", "MedicineController", "TreatmentController")
 
 foreach ($test in $tests) {
-    dotnet-coverage collect "dotnet test --filter 'FullyQualifiedName~DucksNet.IntegrationTests.$($test)Tests'" -f xml -o "tests/DucksNet.IntegrationTests/$($test).coverage.xml"
+    dotnet-coverage collect "dotnet test --filter 'FullyQualifiedName~DucksNet.IntegrationTests.$($test)Tests'" -f xml -o "$($test).coverage.xml"
 }
+dotnet-coverage merge --remove-input-files "DucksNet.IntegrationTests/*.coverage.xml" -f xml -o "coverage.xml"
+
