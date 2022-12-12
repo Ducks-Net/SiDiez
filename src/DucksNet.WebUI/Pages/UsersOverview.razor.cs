425f3e6a2532965d8d3692b4ff785d5ed33cda11
OpariucRares <46899212+OpariucRares@users.noreply.github.com>
2022-12-12 12:38:52 +0200

    removed for sonarqube

--- src/DucksNet.WebUI/Pages/UsersOverview.razor.cs
+++ src/DucksNet.WebUI/Pages/UsersOverview.razor.cs
@@ -9,28 +9,4 @@ public partial class UsersOverview : ComponentBase
     [Inject]
     public IUserDataService? UserDataService { get; set; }
     public List<User> Users { get; set; } = default!;
-
-    protected async Task ReloadAllUsers()
-    {
-        Users = (await UserDataService!.GetAllUsers()).ToList();
-        if (Users.Count == 0)
-        {
-            Users = default!;
-        }
-    }
-
-    protected async Task CreateUser(CreateUserModel userCreateModel)
-    {
-        await UserDataService!.CreateUser(userCreateModel);
-    }
-
-    protected async Task UpdateUser(string userId, UpdateUserModel updateuserModel)
-    {
-        await UserDataService!.UpdateUser(userId, updateuserModel);
-    }
-
-    protected async Task DeleteUser(string userId)
-    {
-        await UserDataService!.DeleteUser(userId);
-    }
 }
