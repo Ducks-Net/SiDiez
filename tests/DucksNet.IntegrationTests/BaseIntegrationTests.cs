0724502d66058bd92dd002b15d2334dabd8e2ff0
Aldea Andrei <aldea.andrei.977@gmail.com>
2022-12-18 10:54:29 +0200

    [Tests] Fixed base integration tests failing to construct because if inode resources

--- tests/DucksNet.IntegrationTests/BaseIntegrationTests.cs
+++ tests/DucksNet.IntegrationTests/BaseIntegrationTests.cs
@@ -1,27 +1,53 @@
-﻿using DucksNet.Infrastructure.Prelude;
+﻿using System;
+using System.IO;
+using DucksNet.Infrastructure.Prelude;
 using DucksNet.Infrastructure.Sqlite;
 
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using System.Net.Http;
-
+using Microsoft.AspNetCore.Hosting;
 
 namespace DucksNet.IntegrationTests;
 
-public class BaseIntegrationTests<T> where T : class
+public class BaseIntegrationTests<T> : WebApplicationFactory<Program> where T : class
 {
     protected HttpClient TestingClient { get; private set; }
-    protected WebApplicationFactory<T> TestingFactory { get; private set; }
-    protected BaseIntegrationTests()
+    public BaseIntegrationTests()
     {
         ClearDatabase();
-        TestingFactory = new WebApplicationFactory<T>().WithWebHostBuilder(builder => {
-            builder.ConfigureTestServices(services => {
-                services.AddScoped<IDatabaseContext>(provider => new TestDbContext(typeof(T).FullName!));
-            });
-         });
-        TestingClient = TestingFactory.CreateClient();
+        // Use HTTPS by default and do not follow
+        // redirects so they can tested explicitly.
+        ClientOptions.AllowAutoRedirect = false;
+        ClientOptions.BaseAddress = new Uri("https://localhost");
+
+        TestingClient = CreateDefaultClient();
+    }
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureAppConfiguration(configBuilder =>
+        {
+            // Configure the test fixture to write the SQLite database
+            // to a temporary directory, rather than in App_Data.
+            var dataDirectory = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+
+            if (!Directory.Exists(dataDirectory))
+            {
+                Directory.CreateDirectory(dataDirectory);
+            }
+        });
+        
+        // Configure the correct content root for the static content and Razor pages
+        builder.UseSolutionRelativeContentRoot(Path.Combine("src", "DucksNet.API"));
+
+        // Configure the application so HTTP requests related to the OAuth flow
+        // can be intercepted and redirected to not use the real GitHub service.
+        builder.ConfigureServices(services =>
+        {
+            services.AddHttpClient();
+            services.AddScoped<IDatabaseContext, TestDbContext>(_ => new TestDbContext(typeof(T).FullName));
+        });
     }
     
     protected void ClearDatabase()
